d07be0561c78b2e545bf07940ad5ff0d1be8bcea
Eli <elibloch2903@gmail.com>
2025-08-04 17:58:27 +0300

    clean and control

--- src/the_control.py
+++ src/the_control.py
@@ -1,8 +1,8 @@
 from dataAnalyzer import DataAnalyzer
 from clean_data import Clean_data
 from load_data import Load_data
-# from writing_data import DataWriter
-import pandas as pd
+
+
 
 
 class Controller:
@@ -19,12 +19,9 @@ class Controller:
         cleaner.basic_clean()
 
         analyzer = DataAnalyzer(data)
-        results = analyzer.summary_analiza()
-        # writer = DataWriter()
-        # writer.cleaned_data_to_csv(cleaner.data, "results/tweets_dataset_cleaned.csv")
-        # writer.analysis_results_to_json(results, "results/results.json")
 
-        print("Pipeline completed successfully!")
+
+
 
 
 if __name__ == "__main__":
